917fdc83fc1a071df14986bdb3d7e6b85611999f
tskaro <tskaro@gmail.com>
2024-12-17 15:38:29 +0400

    initial structure

--- tasker/admin.py
+++ tasker/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
-from tasker.models import Profile, Task, Habit, CompletedHabit
+from tasker.models import Profile, Task, Habit, CompletedHabit, Category
 
 # Register your models here.
 admin.site.register(Profile)
 admin.site.register(Task)
 admin.site.register(Habit)
 admin.site.register(CompletedHabit)
+admin.site.register(Category)
